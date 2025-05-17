7d001be60b1131db03a35ba75d39955977076e62
Noman Sanaullah <nomansanaullah1@gmail.com>
2025-05-17 13:57:34 +0100

    update docs - pipeline improvements

--- docs/architecture.md
+++ docs/architecture.md
@@ -27,3 +27,47 @@ The deployment architecture is designed to be extensible and customizable. Some 
 - Modifying the JCasC scripts to include additional configuration
 - Adding more namespaces or resources to the `deploy.sh` script
 - Integrating with other tools and services by adding appropriate Helm charts and configurations
+
+## Shared Libraries
+
+### 1. **Checkout**
+
+Checks out the source code from the repository defined in the Jenkins job.
+
+### 2. **Build**
+
+Runs inside a `nodejs` container. Installs Node.js dependencies located in the `app` directory.
+
+### 3. **Test**
+
+Executes unit tests using `npm test` inside the `nodejs` container.
+
+### 4. **Docker Build & Push**
+
+Uses the `docker` container to:
+  - Build a Docker image from the `app` directory.
+  - Tag the image with the build number.
+  - Push the image to Docker Hub using stored credentials. Its able to use the same code to push to ECR as well by replacing the `url` in `withDockerRegistry`
+
+### 5. **Lint Application Helm Chart**
+
+Uses the `kube-tools` container to:
+  - Lint the application helm chart located in the `helm` directory.
+
+
+### 6. **Deploy to EKS**
+
+Uses the `kube-tools` container to:
+  - Deploy the application to an EKS cluster using Helm.
+  - The Helm chart is located in the `helm` directory.
+  - The image name is dynamically set using the pushed Docker image.
+  - The same commands can be used to deploy the helm chart to EKS as well by providing `--kubeconfig` to the helm command so it can deploy to EKS
+
+### 7. **Post Actions**
+
+On success or failure, logs a message. Slack notifications are included but commented out. You can enable them by configuring the appropriate Slack credentials and uncommenting the lines.
+
+## Improvements
+
+    - The clusterRoleBinding command allows the service account to act as cluster admin. Its not a recommended policy and this is only used for this particular demo. In a live environment this should be restricted. 
+    - Secret Management through AWS Secrets Manager or Vault instead of harcoding the values in `jenkins-values`. 
--- vars/stdPipeline.groovy
+++ vars/stdPipeline.groovy
@@ -3,7 +3,7 @@ def call(Map config = [:]) {
         agent any
         environment {
             AWS_REGION = 'us-east-1'
-            ECR_REPO = "nomis1/ns1"
+            ECR_REPO = 'nomis1/ns1'
             IMAGE_TAG = "sample-node-app_${env.BUILD_NUMBER}"
             PUSHED_IMAGE = "${ECR_REPO}:${IMAGE_TAG}"
         }
@@ -15,63 +15,77 @@ def call(Map config = [:]) {
             }
             stage('Build') {
                 steps {
-                    container('nodejs') {
-                        sh 'cd app && npm install'
+                    dir('app') {
+                        container('nodejs') {
+                            sh 'npm install'
+                        }
                     }
                 }
             }
             stage('Test') {
                 steps {
-                    container('nodejs') {
-                        sh 'cd app && npm test'
+                    dir('app') {
+                        container('nodejs') {
+                            sh 'npm test'
+                        }
                     }
                 }
-            }            
+            }
             stage('Docker Build & Push') {
                 steps {
-                    container('docker') {
-                        withDockerRegistry([credentialsId: 'dockerhub', url: 'https://index.docker.io/v1/']) {
+                    dir('app') {
+                        container('docker') {
+                            withDockerRegistry([credentialsId: 'dockerhub', url: 'https://index.docker.io/v1/']) {
+                                sh '''
+                                docker build . -t ${IMAGE_TAG}
+                                docker tag ${IMAGE_TAG} ${ECR_REPO}:${IMAGE_TAG}
+                                docker push ${ECR_REPO}:${IMAGE_TAG}
+                                '''
+                            }
+                        }
+                    }
+                }
+            }
+            stage('Helm Lint') {
+                steps {
+                    dir('helm') {
+                        container('kube-tools') {
                             sh '''
-                              cd app
-                              docker build . -t ${IMAGE_TAG}
-                              docker tag ${IMAGE_TAG} ${ECR_REPO}:${IMAGE_TAG}
-                              docker push ${ECR_REPO}:${IMAGE_TAG}
+                            helm lint . -f ./values.yaml
                             '''
                         }
-
                     }
-                    echo 'Simulate push to ECR or implement AWS CLI commands here'
                 }
             }
             stage('Deploy to EKS') {
                 steps {
-                    container('kube-tools') {
-                        sh '''
-                        cd helm
-                        helm upgrade -f ./values.yaml --install sample-node-app . --set imageName=${PUSHED_IMAGE} -n applications
-                        '''
-
+                    dir('helm') {
+                        container('kube-tools') {
+                            sh '''
+                            helm upgrade -f ./values.yaml --install sample-node-app . --set imageName=${PUSHED_IMAGE} -n applications
+                            '''
+                        }
                     }
                 }
             }
         }
         post {
             success {
-                    echo "Deployment Success!"
-                    // slackSend(teamDomain: 'team-domain',
-                            // tokenCredentialId: 'slack-jenkins-id',
-                            // baseUrl: 'https://<team-domain>.slack.com/services/hooks/jenkins-ci/',
-                            // channel: '#jenkins',color: '#e34234',
-                            // message: "${MS_NAME} has been deployed to env ${env.BRANCH_NAME}")    
+                    echo 'Deployment Success!'
+            // slackSend(teamDomain: 'team-domain',
+            // tokenCredentialId: 'slack-jenkins-id',
+            // baseUrl: 'https://<team-domain>.slack.com/services/hooks/jenkins-ci/',
+            // channel: '#jenkins',color: '#e34234',
+            // message: "${MS_NAME} has been deployed to env ${env.BRANCH_NAME}")
             }
             failure {
-                    echo "Deployment Failed!"
-                    // slackSend(teamDomain: 'team-domain',
-                            // tokenCredentialId: 'slack-jenkins-id',
-                            // baseUrl: 'https://<team-domain>.slack.com/services/hooks/jenkins-ci/',
-                            // channel: '#jenkins',color: '#e34234',
-                            // message: "Deployment Failed! ${MS_NAME} to env ${env.BRANCH_NAME}")    
-            }		
+                    echo 'Deployment Failed!'
+            // slackSend(teamDomain: 'team-domain',
+            // tokenCredentialId: 'slack-jenkins-id',
+            // baseUrl: 'https://<team-domain>.slack.com/services/hooks/jenkins-ci/',
+            // channel: '#jenkins',color: '#e34234',
+            // message: "Deployment Failed! ${MS_NAME} to env ${env.BRANCH_NAME}")
+            }
         }
     }
 }
